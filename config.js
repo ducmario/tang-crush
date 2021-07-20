bc11014e26454b3ab82fceb581290122ca53a71e
ducmario <86781667+ducmario@users.noreply.github.com>
2021-07-20 15:21:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/u.are.my.shine/
+    messLink: 'https://www.facebook.com/u.are.my.shine'
 }
